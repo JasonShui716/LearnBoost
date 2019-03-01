3ce9ffc26e20536218b60e26fd43bc2547c7f779
Jiaxin Shui <jxshui@yitu-inc.intra>
2019-03-01 18:52:01 +0800

    ignore

---
+++ cmake-build-debug/CMakeFiles/LearnBoost.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/daytime1-client.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/daytime1-client.cpp.o"
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/daytime2-server.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/daytime2-server.cpp.o"
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/timer1.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/timer1.cpp.o"
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/timer2.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/timer2.cpp.o"
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/timer3.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/timer3.cpp.o"
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/timer4.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/timer4.cpp.o"
+  "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/asio/timer5.cpp" "/home/jxshui/workspace/jasonsui/cpp/LearnBoost/cmake-build-debug/CMakeFiles/LearnBoost.dir/asio/timer5.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LearnBoost.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnBoost.dir/asio/daytime1-client.cpp.o"
+  "CMakeFiles/LearnBoost.dir/asio/daytime2-server.cpp.o"
+  "CMakeFiles/LearnBoost.dir/asio/timer1.cpp.o"
+  "CMakeFiles/LearnBoost.dir/asio/timer2.cpp.o"
+  "CMakeFiles/LearnBoost.dir/asio/timer3.cpp.o"
+  "CMakeFiles/LearnBoost.dir/asio/timer4.cpp.o"
+  "CMakeFiles/LearnBoost.dir/asio/timer5.cpp.o"
+  "LearnBoost.pdb"
+  "LearnBoost"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnBoost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
